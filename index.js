0be58302503381d8d652fbbbcd619b241d87c649
Avinash Verma <avinash.verma@oradian.com>
2021-03-10 11:59:11 +0100

    Add reference to color

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 var xml = require('xml');
 var Base = require('mocha').reporters.Base;
+var color = Base.color;
 var fs = require('fs');
 var path = require('path');
 var debug = require('debug')('mocha-junit-reporter');
